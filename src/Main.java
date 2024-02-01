147f8626f3fb7af24e5c05c0a35b4b6480d079a1
SUSHAN BANIYA <sushanbaniya@gmail.com>
2024-02-01 20:11:31 +0545

    Basic calculator completed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        // console calculator
+        int result = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first Number: ");
+        String n1 = sc.nextLine();
+        int num1 = Integer.parseInt(n1);
+
+        System.out.println("Enter operator: ");
+        String op = sc.nextLine();
+
+        System.out.println("Enter second Number: ");
+        String n2 = sc.nextLine();
+        int num2 = Integer.parseInt(n2);
+
+        if(op.equals("+")) {
+            result = num1 + num2;
+        } else if(op.equals("-")) {
+            result = num1 - num2;
+        } else if(op.equals("*")) {
+            result = num1 * num2;
+        } else if(op.equals("/")) {
+            result = num1 / num2;
+        } else {
+            System.out.println("Invalid Operator");
+        }
+
+        System.out.println("The Result of " + num1 + " " +  op + " " + num2 + " is: " + result);
 
 
 
